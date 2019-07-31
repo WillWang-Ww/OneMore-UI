c02f5883724950aff2a6809c9c3388d90596446f
WILLWANG <wangziweichn@outlook.com>
2019-07-31 21:57:25 +0800

    添加三个测试用例：Icon/loading/check

--- src/app.js
+++ src/app.js
@@ -10,3 +10,56 @@ new Vue({
         check1:false,
     }
 })
+
+import chai from 'chai'
+
+const expect = chai.expect
+//单元测试
+
+//测试Button中添加固定Icon
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData:{
+            icon: 'setting'
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#icon-setting')
+    vm.$el.remove()
+    vm.$destroy()
+}
+//测试Button中Icon与Loading的切换
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData:{
+            icon: 'setting',
+            loading:true
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#icon-loading')
+    vm.$el.remove()
+    vm.$destroy()
+}
+//测试Button中Icon与Check的切换
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData:{
+            icon: 'setting',
+            check:true
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#icon-check')
+    vm.$el.remove()
+    vm.$destroy()
+}
